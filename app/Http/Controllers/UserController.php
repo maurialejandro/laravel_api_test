72c33c97fe2b07dd66bf6f5f76707dbddff4c61e
mauri.alejandro2165@gmail.com <mauri.alejandro2165@gmail.com>
2021-08-28 16:16:21 -0400

    Creacion de registro usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,30 +10,41 @@ class UserController extends Controller
 {
     public function register(Request $request){
         $json = json_decode($request->getContent());
-        
-        if (empty($json->name) && empty($json->email) && empty($json->password)){// valida que no venga vacio
-            return "Nombre, email y contraseña son requeridos";
-        } else if (empty($json->name) && empty($json->password)) {
-            return "Nombre y contraseña son requerido";
-        } else if (empty($json->email) && empty($json->password)){
-            return "Correo y contraseña son requeridos";
-        } else if (empty($json->name) && empty($json->email)){
-            return "Nombre y correo son requeridos";
-        } else if (empty($json->name)){
-            return "Nombre es requerido";
-        }else if (empty($json->email)){
-            return "Correo es requerido";
-        }else if (empty($json->password)){
-            return "Password es requerida";
+        if (!empty($json->name) && !empty($json->email) && !empty($json->password)){
+
+            $name = (!is_null($json->name) && isset($json->name)) ? $json->name : null;
+            $email = (!is_null($json->email) && isset($json->email)) ? $json->email : null;
+            $pass = (!is_null($json->password) && isset($json->password)) ? $json->password : null;
+            $user = new User();
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = hash('sha256', $pass);
+            $isset_user = User::where('email' , '=', $email)->first();
+            
+            if(empty($isset_user)){
+                $user->save();
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'User creado'
+                );
+            } else {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'User ya creado'
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'User NO creado'
+            );
         }
-
-        $name = (!is_null($json->name) && isset($json->name)) ? $json->name : null;
-        $email = (!is_null($json->email) && isset($json->email)) ? $json->email : null;
-        $pass = (!is_null($json->password) && isset($json->password)) ? $json->password : null;
         
-
-
-        return 1;      
+        return $data;      
     }
 
 }
