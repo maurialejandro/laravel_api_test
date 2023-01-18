2ad4e6380911b05c3921a0225c1cc855a6a53d6d
flyingbiker <mauri.alejandro2165@gmail.com>
2023-01-18 12:36:55 -0300

    cambios

--- database/migrations/2021_08_29_205550_create_platos_table.php
+++ database/migrations/2021_08_29_205550_create_platos_table.php
@@ -13,22 +13,22 @@ class CreatePlatosTable extends Migration
      */
     public function up()
     {
-        //Schema::create('platos', function (Blueprint $table) {
-        //    $table->Increments('id');
-        //    $table->Integer('user_id');
-        //    $table->foreign('user_id')->references('id')->on('users');
-        //    $table->string('name');
-        //    $table->integer('price');
-        //    $table->string('description');
-        //    $table->string('img');
-        //    $table->string('latitude');
-        //    $table->string('longitude');
-        //    $table->timestamps();
-        //});
-        // Agregar rating, ratingTotal, quantityVoting sin que se elimine la tabla y sus datos solo agregar los campos mencionados.
-        Schema::table('platos', function (Blueprint $table){
-            $table->boolean('is_favorite')->default(0);
+        Schema::create('platos', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->integer('price');
+            $table->string('description');
+            $table->string('img');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
         });
+        // Agregar rating, ratingTotal, quantityVoting sin que se elimine la tabla y sus datos solo agregar los campos mencionados.
+        //Schema::table('platos', function (Blueprint $table){
+        //    $table->boolean('is_favorite')->default(0);
+        //});
         
     }
 
@@ -37,8 +37,8 @@ class CreatePlatosTable extends Migration
      *
      * @return void
      */
-    //public function down()
-    //{
-    //    Schema::dropIfExists('platos');
-    //}
+    public function down()
+    {
+        Schema::dropIfExists('platos');
+    }
 }
